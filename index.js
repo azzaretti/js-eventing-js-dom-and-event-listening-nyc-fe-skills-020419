abd39b0d8d8f0445131810254f60b3d5f93863ff
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-04-08 01:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,26 +6,69 @@
 //   console.log(event)
 // }
 
-// function onKeyDown(){
-//   console.log('Hey! You clicked key number ' + event.which + ". Nice Job!")
-// }
-
-//event listener needs two inputs 1st, action 2nd function to perform
-// main.addEventListener('click', onClick)
-// document.addEventListener('keydown', onKeyDown)
-
-
-let inputBox = document.getElementById('inputBox')
-
-
-
-function onInputKeyDown(event) {
-  if (event.which === 71) {
-  event.preventDefault()
-} else {
+function onKeyDown(){
   console.log(event.which)
 }
-}
 
-inputBox.addEventListener('keydown', onInputKeyDown)
+// //event listener needs two inputs 1st, action 2nd function to perform
+// // // main.addEventListener('click', onClick)
+document.addEventListener('keydown', onKeyDown)
+
+
+// // let inputBox = document.getElementById('inputBox')
+
+
+
+// // function onInputKeyDown(event) {
+// //   if (event.which === 71) {
+// //   event.preventDefault()
+// // } else {
+// //   console.log(event.which)
+// // }
+// // }
+
+// // inputBox.addEventListener('keydown', onInputKeyDown)
+
+// const p = document.getElementById('yellow')
+// const b = document.getElementById('blue')
+// const r = document.getElementById('red')
+// const o = document.getElementById('orange')
+// const g = document.getElementById('green')
+// const pink = document.getElementById('pink')
+// // function onYellowClick(event, )
+
+// p.addEventListener('click', () => { 
+//   p.style.backgroundColor = 'yellow'
+//   b.style.backgroundColor = 'blue'
+//   o.style.backgroundColor = 'orange'
+//   g.style.backgroundColor = 'green'
+//   r.style.backgroundColor = 'red'
+//   pink.style.backgroundColor = 'pink'
+// })
+
+// p.addEventListener('dblclick', () => {
+//   p.style.backgroundColor = 'white'
+//   b.style.backgroundColor = 'white'
+//   o.style.backgroundColor = 'white'
+//   g.style.backgroundColor = 'white'
+//   r.style.backgroundColor = 'white'
+//   pink.style.backgroundColor = 'white'
+// })
+
+// let button = document.getElementById('plus')
+
+// // button.addEventListener('click', () => {
+// //   const newP = document.createElement('p')
+// //   newP.innerHTML = 'Congratulations! You did it!'
+// //   document.body.appendChild(newP)
+// // })
+
+// const divs = document.querySelectorAll('div')
+
+// for(let div of divs) {
+//   div.addEventListener('click', (event) => {
+//     div.style.backgroundColor = "blue"
+//     event.stopPropagation()
+//   })
+// }
 
